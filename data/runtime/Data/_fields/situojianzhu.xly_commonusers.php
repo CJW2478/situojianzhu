2724fcfd8eef3b5ec5e38dec82a8a282b0e66549
heart2world <njm@heart2world.com>
2018-08-31 11:42:39 +0800

    缓存

--- data/runtime/Data/_fields/situojianzhu.xly_commonusers.php
+++
@@ -1,1 +0,0 @@
-a:8:{i:0;s:2:"id";i:1;s:6:"mobile";i:2;s:9:"user_name";i:3;s:9:"user_duty";i:4;s:6:"openid";i:5;s:4:"type";i:6;s:6:"status";s:5:"_type";a:7:{s:2:"id";s:10:"bigint(20)";s:6:"mobile";s:11:"varchar(11)";s:9:"user_name";s:11:"varchar(50)";s:9:"user_duty";s:12:"varchar(100)";s:6:"openid";s:11:"varchar(50)";s:4:"type";s:10:"tinyint(4)";s:6:"status";s:10:"tinyint(4)";}}
